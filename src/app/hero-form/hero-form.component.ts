1570f60ae9cf3c459c234d5efc470d3a45799951
Assaf <assaf.tenen@gmail.com>
2016-10-16 23:48:09 +0300

    close

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -6,6 +6,7 @@ import { Hero }    from './../hero';
   templateUrl: 'hero-form.component.html'
 })
 export class HeroFormComponent {
+  active: boolean = true;
   powers = ['Really Smart', 'Super Flexible',
             'Super Hot', 'Weather Changer'];
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
@@ -13,4 +14,10 @@ export class HeroFormComponent {
   onSubmit() { this.submitted = true; }
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+  newHero() {
+   this.model = new Hero(42, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+}
+
 }
